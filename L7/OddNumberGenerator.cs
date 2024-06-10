e3c3bf3dce63fb8b924ceaeffc11107368bedc98
Alexandr Nosachev <nosachev2512@gmail.com>
2024-06-11 00:09:45 +0300

    7th homework

---
+++ L7/OddNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace L7
+{
+    public class OddNumberGenerator
+    {
+        private int _current;
+
+        public OddNumberGenerator()
+        {
+            _current = 1;
+        }
+
+        public int Next()
+        {
+            int current = _current;
+            _current = current + 2;
+            return current;
+        }
+    }
+}
